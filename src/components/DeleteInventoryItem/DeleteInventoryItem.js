1957c0d8ee267792e41dc23a9014aeb9ba3f0026
Mario Miralles <miralles.mario@gmail.com>
2024-03-27 22:11:06 -0400

    created delete inventory item components + styling. consolidated both delete modals styling into one file

--- src/components/DeleteInventoryItem/DeleteInventoryItem.js
+++ src/components/DeleteInventoryItem/DeleteInventoryItem.js
@@ -1,8 +1,8 @@
-import '../../styles/components/DeleteModal.scss';
+import './DeleteInventoryItem.scss';
 import close from '../../assets/icons/close-24px.svg';
 import deleteIcon from '../../assets/icons/delete_outline-24px.svg'
 import React, { useState } from 'react';
-function DeleteWarehouse() {
+function DeleteInventoryItem() {
     // Declare variable for useState
     const [modal, setModal] = useState(true); // Change to false once the icon button
     {/* <img className='delete__icon' src={deleteIcon} alt='delete button' onClick={toggleModal} /> */}
@@ -19,8 +19,8 @@ function DeleteWarehouse() {
                     <article className='delete__modal'>
                         <section className='delete__container'>
                             <img className='delete__close' src={close} alt='close button' onClick={toggleModal} />
-                            <h1 className='delete__heading'>Delete Washington warehouse?</h1>
-                            <h5 className='delete__body'>Please confirm that you'd like to delete Washington from the list of warehouses. You won't be able to undo this action.</h5>
+                            <h1 className='delete__heading'>Delete Television inventory item?</h1>
+                            <h5 className='delete__body'>Please confirm that you'd like to delete Television from the inventory list. You won't be able to undo this action.</h5>
                         </section>
                         <section className='delete__container-buttons'>
                             <button className='delete__cancel-button' onClick={toggleModal}><h3 className='delete__cancel-button--heading'>Cancel</h3></button>
@@ -32,4 +32,4 @@ function DeleteWarehouse() {
         </>
     )
 }
-export default DeleteWarehouse;
+export default DeleteInventoryItem;
